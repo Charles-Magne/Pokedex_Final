b837e642ffe4795a08f766221c9ce39bfd3fb3d5
laurent <laurent.supplisson@gmail.com>
2022-10-28 17:12:11 +0200

    page team presque ok

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,26 +1,37 @@
-import Provisoire from '../../assets/pictures/face-a-face.jpg';
+import Provisoire from "../../assets/pictures/face-a-face.jpg";
 
 import "./style.scss";
 
 function Team() {
   return (
-  <div>
-    <span>Notre super équipe </span>
-    <img className="pictureCio" src={ Provisoire } />
-    <span>C.I.O</span>
-    <p>Son parcours</p>
-    <img className="pictureCio" src={ Provisoire } />
-    <span>C.I.O</span>
-    <p>Son parcours</p>
-    <span>Et un remerciment tout particulier a Tony </span>
-    {/* Titre
+    <div>
+      <span className="titreMain">Notre super équipe </span>
+      <div className="theTeam">
+        <div className="role">
+          <img className="picture" src={Provisoire} />
+          <div className="description"  >
+            <span className="roleTitre" >Madame Effie Directrice Géneral</span>
+            <p>Developpeuse senior Full-stack et amatrice de fromage blanc </p>
+          </div>
+        </div>
+        <div className="role" >
+          <img className="picture" src={Provisoire} />
+          <div className="description" >
+          <span className="roleTitre" >Laurent</span>
+          <p>Developpeur front-end junior</p>
+          </div>
+        </div>
+
+        <span>Et un remerciment tout particulier a Tony </span>
+        {/* Titre
     petite presentation
     Pourquoi ce projet ?
     img chat
       img moi
       img tony
       lien vers linkedin */}
-  </div>
+      </div>
+    </div>
   );
 }
 
